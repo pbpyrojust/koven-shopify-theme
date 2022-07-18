cd872f6fb22eaa15f67cd50cc52f987853770f4c
Justin Adams <justinadams@Justins-MacBook-Pro-2.local>
2022-07-18 13:31:42 -0700

    Updated gulpefile. js to the new gulp-sass syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
-// Set the required modules
-var gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-var concat = require('gulp-concat');
 
-// Set the sass compiler
-sass.compiler = require('node-sass');
+// Set the required modules
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
 // Gulp sass task to look for .scss files and compile to .css
-gulp.task('sass', function () {
-   return gulp.src('/sass/**/*.scss')
-   .pipe(concat('justin.scss'))
-   .pipe(sass().on('error', sass.logError))
-   .pipe(gulp.dest('/assets/'));
-});
+function buildStyles() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets'));
+};
 
 // Gulp sass:watch task to always compile .scss to .css on save
-gulp.task('sass:watch', function () {
-    gulp.watch('/sass/**/*.scss', ['sass']);
- });
+function buildStyles() {
+    return gulp.src('./sass/**/*.scss')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(gulp.dest('./assets'));
+  };
+  
+exports.buildStyles = buildStyles;
