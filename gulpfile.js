a13f08c97a8645a4dedd4c01d52b8bc110ea7f67
Justin Adams <justinadams@Justins-MacBook-Pro-2.local>
2022-07-18 13:10:55 -0700

    Adding comments to the gulp file to see what I did

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 'use strict';
+// Set the required modules
 var gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 var concat = require('gulp-concat');
 
+// Set the sass compiler
 sass.compiler = require('node-sass');
 
+// Gulp sass task to look for .scss files and compile to .css
 gulp.task('sass', function () {
    return gulp.src('/sass/**/*.scss')
    .pipe(concat('justin.scss'))
@@ -12,6 +15,7 @@ gulp.task('sass', function () {
    .pipe(gulp.dest('/assets/'));
 });
 
+// Gulp sass:watch task to always compile .scss to .css on save
 gulp.task('sass:watch', function () {
     gulp.watch('/sass/**/*.scss', ['sass']);
  });
